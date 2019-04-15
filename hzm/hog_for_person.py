5e3e02ef88c8a792e12781493b68b0a1e0c4bdfc
Malakamalaka <42467088+Malakamalaka@users.noreply.github.com>
2019-04-15 17:06:40 +0800

    hog_feature

---
+++ hzm/hog_for_person.py
@@ -0,0 +1,33 @@
+from skimage.feature import hog
+import os
+import torch
+from skimage import transform
+from skimage import img_as_ubyte
+from skimage import io
+import matplotlib.pyplot as plt
+import numpy as np
+def get_hog_tensor():
+    length=len(os.listdir(r"C:\\Users\hzm\Downloads\Yale-FaceRecognition-master\imgs"))
+    hog_tensor=torch.empty(length,1,100,100)
+    sim_mat=torch.empty(length,length)
+    for i in range(length):
+        print(i)
+        im = io.imread("C:\\Users\hzm\Downloads\Yale-FaceRecognition-master\imgs\s"+str(i+1)+".bmp",as_gray=True)
+        normalised_blocks, hog_image = hog(im, orientations=9, pixels_per_cell=(2, 2), cells_per_block=(2, 2),
+                                          block_norm = 'L1',transform_sqrt = True,visualize=True)
+        #io.imshow(hog_image)
+        #plt.show()
+        im_tensor = torch.tensor(hog_image)
+        mm=torch.max(im_tensor)
+        mn=torch.min(im_tensor)
+        distance=mm-mn
+        for j in range(100):
+            for k in range(100):
+                im_tensor[j,k]=255*(im_tensor[j,k]-mn)/distance
+        hog_tensor[i,0,:,:]=im_tensor
+        for l in range(length):
+            if i//11==l//11:
+                sim_mat[i,l]=1
+            else:
+                sim_mat[i,l]=-1/14
+    return hog_tensor,sim_mat
